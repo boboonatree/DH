217c386ffb57188daf4adf79208f709b10ece2b0
Duo <63012562+DuoM2@users.noreply.github.com>
2022-05-31 23:14:41 -0400

    Create pokedex.ts

---
+++ data/mods/bobolieffakemon/pokedex.ts
@@ -0,0 +1,16 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+  	breroot: {
+		  num: -100,
+  		name: "Breroot",
+  		types: ["Grass", "Ground"],
+  		genderRatio: {M: 0.5, F: 0.5},
+  		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 60, spe: 60},
+  		abilities: {0: "Storm Drain", H: "Ripen"},
+  		heightm: 1.8,
+  		weightkg: 900,
+  		color: "Brown",
+//  		prevo: "Crecebayo",
+  		evoLevel: 32,
+  		eggGroups: ["Field", "Grass"],
+  	},
+};
