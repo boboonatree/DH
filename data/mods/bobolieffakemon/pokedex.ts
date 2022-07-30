551ae0fe36d3f8ebc8895d9bcb6a5caf4d7a9bf2
boboonatree <103380682+boboonatree@users.noreply.github.com>
2022-07-30 14:48:30 -0500

    Update pokedex.ts

--- data/mods/bobolieffakemon/pokedex.ts
+++ data/mods/bobolieffakemon/pokedex.ts
@@ -4,7 +4,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
   		name: "Breroot",
   		types: ["Grass", "Ground"],
   		genderRatio: {M: 0.5, F: 0.5},
-  		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 60, spe: 60},//
+  		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 60, spe: 60},
   		abilities: {0: "Storm Drain", H: "Ripen"},
   		heightm: 1.8,
   		weightkg: 900,
